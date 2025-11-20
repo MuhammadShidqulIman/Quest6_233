d93496c533fe57c49908511e87fbf3961e4e5549
MuhammadShidqulIman <muhammadshidqul3@gmail.com>
2025-11-20 14:50:43 +0700

    Menambah navigasi awal dan struktur NavHost pada SiswaApp

---
+++ app/src/main/java/com/example/myarsitektur/View/uicontroller/Navigasi.kt
@@ -0,0 +1,41 @@
+package com.example.myarsitektur.view.uicontroller
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.compose.ui.platform.LocalContext
+import com.example.myarsitektur.model.DataJK.JenisK
+
+import com.example.myarsitektur.view.FormSiswa
+import com.example.myarsitektur.view.TampilSiswa
+import com.example.myarsitektur.viewmodel.SiswaViewModel
+
+
+enum class Navigasi {
+    Formulir,
+    Detail
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SiswaApp(
+    navController: NavHostController,
+    viewModel: SiswaViewModel
+) {
+
+    val uiState = viewModel.statusUI.collectAsState()
+
+    Scaffold { isiRuang ->
+
+        NavHost(
+            navController = navController,
+            startDestination = Navigasi.Formulir.name,
+            modifier = Modifier.padding(isiRuang)
+        ) {
+
